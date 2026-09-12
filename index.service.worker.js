09ca8d41d26d59a2c0ec83691e3afc364c2f6a92
Jankinc <86422441+Jankinc@users.noreply.github.com>
2026-09-13 01:24:38 +0800

    9

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789233463|2012886442';
+const CACHE_VERSION = '1789233815|2364592923';
 /** @type {string} */
 const CACHE_PREFIX = 'HeiPoomsae-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
